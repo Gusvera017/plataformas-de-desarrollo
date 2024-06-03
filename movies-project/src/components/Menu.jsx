871164417947dc4b62c390bf209cfed25c17264e
Gustavo Vera <gusvera.017@gmail.com>
2024-06-02 21:54:53 -0300

    borro hipervinculos del Menu

--- movies-project/src/components/Menu.jsx
+++ movies-project/src/components/Menu.jsx
@@ -11,13 +11,13 @@ const Menu = () => {
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item href="#">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#">
                                 Another action
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
+                            <NavDropdown.Item href="#">
                                 Separated link
                             </NavDropdown.Item>
                         </NavDropdown>

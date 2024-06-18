0856d865edb58c2f6a5ea5feafe8b47d01fe7b02
Gustavo Vera <gusvera.017@gmail.com>
2024-06-18 20:55:25 -0300

    se separaron las pages en carpetas

---
+++ movies-project/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className="container-home">
+      <div className="title-home">
+        <h1>BIENVENID#! a The Movies Project</h1>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Home;
--- movies-project/src/pages/Login/Login.jsx
+++ movies-project/src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import './Login.css';
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {

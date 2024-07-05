96dd3641bf6c1e3453b81c4745d69d465b20c7d4
Gustavo Vera <gusvera.017@gmail.com>
2024-07-04 21:51:10 -0300

    Elimino la prueba de buscador (entrega final)

--- movies-project/src/components/Menu.jsx
+++ movies-project/src/components/Menu.jsx
@@ -20,7 +20,6 @@ const Menu = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">The Movies Project</Link>
-        <Searcher />
         <div className="d-flex justify-content-flex-end">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {(userAuthenticated && adminAuthenticated) &&

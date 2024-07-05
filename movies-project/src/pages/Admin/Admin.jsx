caba00c4d646e886dac388092ab50a3f9bd53a89
Gustavo Vera <gusvera.017@gmail.com>
2024-07-04 21:58:57 -0300

    mensaje en pantalla admin

--- movies-project/src/pages/Admin/Admin.jsx
+++ movies-project/src/pages/Admin/Admin.jsx
@@ -6,6 +6,12 @@ const Admin = () => {
       <div className="title-admin mb-5">
         <h1>BIENVENID#! al Panel de Administrador</h1>
       </div>
+      <div className="d-flex justify-content-center">
+        <p style={{ margin: '120px', fontSize: '50px' }}>DISPONIBLE PROXIMAMENTE... </p>
+      </div>
+      <div className="d-flex justify-content-center">
+        <p style={{ fontSize: '20px' }}>Administración de usuarios </p>
+      </div>
     </div>
 
   )

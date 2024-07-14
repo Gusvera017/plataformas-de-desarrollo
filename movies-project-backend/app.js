69e19755bc5e08f7c3035382980b69701eaeb04c
Gustavo Vera <gusvera.017@gmail.com>
2024-07-14 19:29:38 -0300

    variables de entorno con dotenv y pruebas

--- movies-project-backend/app.js
+++ movies-project-backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const pool = require('./db');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 8888;
